2bb44e50d8c2652702213a9eac7e4fb151f126d1
ikzsl <ikzsl@yandex.ru>
2020-10-13 12:46:39 +0300

    Updates

---
+++ precache-manifest.e9b8490b9b2721548ec9f5ef05d7ed79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde84228f1efaac4947245f01d0f5bc4",
+    "url": "/demiurgsApp/index.html"
+  },
+  {
+    "revision": "1819e52e687e7fc30dd6",
+    "url": "/demiurgsApp/static/css/main.ae106326.chunk.css"
+  },
+  {
+    "revision": "d521e43c651884bfb1b8",
+    "url": "/demiurgsApp/static/js/2.f1985360.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demiurgsApp/static/js/2.f1985360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1819e52e687e7fc30dd6",
+    "url": "/demiurgsApp/static/js/main.338fd491.chunk.js"
+  },
+  {
+    "revision": "c527d1e1cd6d0bb7eae4",
+    "url": "/demiurgsApp/static/js/runtime-main.21bf0fcd.js"
+  }
+]);
